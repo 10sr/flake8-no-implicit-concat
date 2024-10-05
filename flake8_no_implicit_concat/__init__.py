f20281ae6556eca3df3138f202140bf537b8a0af
10sr <8.slashes@gmail.com>
2024-10-05 23:19:58 +0900

    Remove some mypy ignore comments

--- flake8_no_implicit_concat/__init__.py
+++ flake8_no_implicit_concat/__init__.py
@@ -36,11 +36,11 @@ def _is_ok_lt_py312(a: tokenize.TokenInfo, b: tokenize.TokenInfo) -> bool:
 def _is_ok(a: tokenize.TokenInfo, b: tokenize.TokenInfo) -> bool:
     if a.type == b.type == tokenize.STRING:
         return False
-    if a.type == tokenize.STRING and b.type == tokenize.FSTRING_START:  # type: ignore[attr-defined]
+    if a.type == tokenize.STRING and b.type == tokenize.FSTRING_START:
         return False
-    if a.type == tokenize.FSTRING_END and b.type == tokenize.STRING:  # type: ignore[attr-defined]
+    if a.type == tokenize.FSTRING_END and b.type == tokenize.STRING:
         return False
-    if a.type == tokenize.FSTRING_END and b.type == tokenize.FSTRING_START:  # type: ignore[attr-defined]
+    if a.type == tokenize.FSTRING_END and b.type == tokenize.FSTRING_START:
         return False
     return True
 
