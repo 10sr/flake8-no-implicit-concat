72f4a8e54dbad3740f6733af514e3c57e1fc0895
10sr <8.slashes@gmail.com>
2024-10-05 23:05:11 +0900

    Stop excluding NEWLINE token when checking token list  (#88)
    * Add test case which should be OK
    
    * Stop excluding NEWLINE token when checnking token list
    
    NEWLINE token is used only when expressions are separated logically, so
    implicit concats never happen when NEWLINE is between string literals.
    
    https://docs.python.org/3/library/token.html#module-token

---
+++ tests/run_flake8/classdoc.py
@@ -0,0 +1,11 @@
+class Class:
+    """docstring"""
+    """docstring2"""
+    attribute = "value"
+    """docstring3"""
+    """docstring4"""
+    bttribute = "value2"
+
+print(Class.attribute)
+print(Class.bttribute)
+print(Class.__doc__)
