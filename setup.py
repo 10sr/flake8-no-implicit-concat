4b197d366b58eeef1305a990451b8c0835e4989c
10sr <8.slashes@gmail.com>
2024-11-04 16:31:14 +0900

    Update to use setuptools_scm

--- setup.py
+++ setup.py
@@ -5,13 +5,4 @@
 from setuptools import setup
 
 
-def _get_version() -> str:
-    with open("flake8_no_implicit_concat/_version.py") as f:
-        for line in f:
-            if line.startswith("__version__"):
-                delim = '"' if '"' in line else "'"
-                return line.split(delim)[1]
-    raise RuntimeError("Unable to find version string.")
-
-
-setup(version=_get_version())
+setup()
